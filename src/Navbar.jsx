2719a36a7fafd3d21dca9e2323a1240e5e62f4b4
atenbot <aten27sun@gmail.com>
2025-01-30 00:16:32 +0100

    image fix

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 
 
         <div className="flex justify-between items-center sticky top-0 bg-primary px-2 mx-auto z-10 md:px-8 lg:static lg:px-16">
-        <img src="./assets/logos/degenz.jpg" className="w-24 h-auto md:w-32 aspect-w-1 aspect-h-1 lg:w-36" alt="" />
+        <img src="logos\degenz.jpg" className="w-24 h-auto md:w-32 aspect-w-1 aspect-h-1 lg:w-36" alt="" />
         <div className="flex text-white items-center justify-between md:w-[370px] font-spaceMono font-semibold lg:w-[450px]">
         <a className="hidden md:block">Our Story</a>
         <a className="hidden md:block">Features</a>
